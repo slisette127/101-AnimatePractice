28bc084ceb6bc07ecf7ae08f16d0c934385f5e68
HippErger <berger.clayton@gmail.com>
2020-03-12 16:30:19 -0500

    initial commit:

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 101-AnimatePractice
+
+1. In the `index.html` file you'll find a form with a couple inputs: color and animation name. Go ahead and live serve this page and type in something then click "Create New Element". Try "bounce" and "red".
+1. What's happening?
+1. Look at the HTML file and then look at the `main.js` file to see how the values are being changed?
+1. Figure it out yet?
+1. On the `keyup` event, the input fields' values are being saved to the variables at the top of the JS file. When the "Create New Element" button is clicked another function is fired. What's happening there?
+1. Walk through it line by line and ask yourself what's going on? How does this work?
+1. Create a new input that will let the user change the text that's inside the "New Element". How can you do that? What function will you need to create?
+1. Can you see a pattern yet?
+1. Create a new input, that will let the user choose what type of HTML Element will be created next.
+1. **Bonus**: Create ways to add speed, delay, and an option for infinite or not.
---
+++ index.js
@@ -0,0 +1,35 @@
+let animationName = ""
+let elementColor = ""
+
+// Can you use these two variables to change the type and text of the next element that's created?
+let elementType = ""
+let inputtedText = ""
+
+// function that handles the input "animation-name"
+const setAnimationName = (name) => {
+  animationName = name
+  console.log(animationName)
+}
+
+// function that handles the input "element-color"
+const setElementColor = (color) => {
+  elementColor = color
+}
+
+// This function creates a new element and attaches to the parent element: element-collector
+const createNewElement = () => {
+  const elementContainer = document.getElementById("element-collector")
+  
+  // create a new element with default values
+  const newElement = document.createElement("H3")
+  const textNode = document.createTextNode("Hello")
+
+  // Using dot.notation to access the new element's properties
+  newElement.appendChild(textNode)
+  newElement.classList.add("animated", "infinite", animationName)
+  newElement.style.color = elementColor
+
+  // attach the new element to the parent
+  elementContainer.appendChild(newElement)
+}
+
