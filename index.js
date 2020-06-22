b65c5502a39af563fd789bfce1ebc8be8c84c316
HippErger <berger.clayton@gmail.com>
2020-06-22 16:46:06 -0500

    added input button

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let animationName = ""
 let elementColor = ""
+let elementMessage = ""
 
 // Can you use these two variables to change the type and text of the next element that's created?
 let elementType = ""
-let inputtedText = ""
 
 // function that handles the input "animation-name"
 const setAnimationName = (name) => {
@@ -16,13 +16,18 @@ const setElementColor = (color) => {
   elementColor = color
 }
 
+// function that handles the input "element-message"
+const setElementMessage = (message) => {
+  elementMessage = message
+}
+
 // This function creates a new element and attaches to the parent element: element-collector
 const createNewElement = () => {
   const elementContainer = document.getElementById("element-collector")
   
   // create a new element with default values
   const newElement = document.createElement("H3")
-  const textNode = document.createTextNode("Hello")
+  const textNode = document.createTextNode(elementMessage)
 
   // Using dot.notation to access the new element's properties
   newElement.appendChild(textNode)
